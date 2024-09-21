c0231ee7d23913e4b40af9bef194b175eaffcd2c
LogicJohn <johnnie.hernandez@ibm.com>
2024-09-21 18:26:02 -0400

    added gitignores and adjusted nameing

--- modules/foundry_vault/main.tf
+++ modules/foundry_vault/main.tf
@@ -1,6 +1,6 @@
 
-resource "google_compute_instance" "default" {
-  name         = "docker-instance"
+resource "google_compute_instance" "foundry_vault" {
+  name         = "foundry_vault"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
